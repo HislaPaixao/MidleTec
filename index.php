d395042b61c45875e75a7b3a8a789ef1cd19ec12
HislaPaixao <144177392+HislaPaixao@users.noreply.github.com>
2023-10-19 22:36:37 -0300

    Criando página home

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Home</title>
+</head>
+
+<header class="header_menu">
+    <div class="div_menu">
+        <a href="#" class="logo"><img src="assets/img/logo.png" alt="logo" class="logo_img"></a>
+        <nav class="nav_menu">
+            <ul>
+                <li><a href="#">Cadastro</a></li>
+                <li><a href="login/login.php">login</a></li>
+                <li><a href="material/material.php">material</a></li>
+                <li><a href="instituição/instituicao.php">instituição</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<body>
+    <div class="formato_capa">
+        <div class="capa">
+            <div class="box">
+                <div class="box_content">
+                    <img src="assets/img/capa.png" alt="capa" width="750" height="500">
+                </div>
+            </div>
+            <div class="box_2">
+                <h1>BEM VINDO</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci maxime laudantium temporibus dicta dolore libero. Consequuntur fugiat vitae quaerat ipsa officia commodi aperiam rerum quam temporibus, repellat quod necessitatibus ex!</p>
+            </div>
+        </div>
+    </div>
+
+    <section class="blog">
+        <div>
+            <h1>aaaaaaaaaaaaa</h1>
+        </div>
+    </section>
+
+</body>
+
+</html>
---
+++ login/login.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/boot.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/navstyle.css">
+
+    <title>login</title>
+</head>
+
+<body>
+<header class="header_menu">
+    <div class="div_menu">
+
+        <a href="#" class="logo"><img src="../assets/img/m.png" alt="logo" class="logo_img"></a>
+        <div class="spacer"></div>
+        <nav class="nav_menu">
+
+            <ul>
+         
+                    <li><a href="login/login.php">login</a></li>
+                    <li><a href="material/material.php">material</a></li>
+                    <li><a href="instituição/instituicao.php">instituição</a></li>
+                    <li><a href="#">opção4</a></li>
+    
+            </ul>
+        </nav>
+    </div>
+</header>
+
+    <main>
+        <div class="div2 ">
+            <header>
+                <h1>Login</h1>
+                <form method="POST">
+                    <input type="text" name="matricula" placeholder="Aluno" class="size">
+
+                    <input type="password" name="senha" placeholder="Senha" class="size">
+                    <button type="submit"><b>Entrar</b></button>
+                </form>
+                <p class="ali-rig"><a href="#" class="link1">Recuperar Senha</a></p>
+            </header>
+        </div>
+    </main>
+</body>
+
+</html>
